a1ece94429810db1447d906d9d0e64a29b59452f
Kamil Żogło <k.zoglo@t-bull.com>
2021-08-24 11:57:56 +0200

    Tests - predicates:   - counter for an amount of "isEqual" function calls was implemented

--- test/unit/predicates.test.js
+++ test/unit/predicates.test.js
@@ -1,26 +1,39 @@
 const expect = require('chai').expect;
-const { isEqual } = require('../../predicates');
+const sinon = require('sinon');
+
+const predicates = require('../../predicates');
 
 describe('Predicates', () => {
+  const spy = sinon.spy(predicates, 'isEqual');
   const execExpectations = (result) => {
     expect(result).to.be.eq(result);
     expect(result).to.not.be.eq(!result);
   };
 
+  before(() => {
+    spy.resetHistory();
+  });
+
+  afterEach(() => {
+    spy.resetHistory();
+  });
+
   it('should return true if strictly compared elements are the same', () => {
     // NaN can't be compared with itself
     const items = [1, '1', '', true, null, undefined, {}, [], { a: 'a' }];
     items.forEach((elem) => {
-      const result = isEqual(elem, elem);
+      const result = predicates.isEqual(elem, elem);
       execExpectations(result);
     });
+    expect(spy.callCount).to.be.eq(items.length);
   });
 
   it('should return true if strictly compared elements are different', () => {
     const items = [1, '1', '', true, null, undefined, {}, [], { a: 'a' }];
     items.forEach((elem) => {
-      const result = isEqual(elem, !elem);
+      const result = predicates.isEqual(elem, !elem);
       execExpectations(result);
     });
+    expect(spy.callCount).to.be.eq(items.length);
   });
 });
