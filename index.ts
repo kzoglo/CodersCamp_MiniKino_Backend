dafcfa95fa3f549d0a8faf4fb27ed9ef34ba28ea
Kamil Żogło <zoglokamil@gmail.com>
2021-09-12 13:56:47 +0200

    TS transition - app main point: - new starting app point waiting for creation, - some adjustment in 'index.ts' to work with application under construction

--- index.ts
+++ index.ts
@@ -1,20 +1,33 @@
 import mongoose from 'mongoose';
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
 import config from 'config';
-const { dbName, portNb, host, password } = config.get('db');
+// const { dbName, portNb, host, password } = config.get('db');
+interface IDbConfig {
+  dbName: string;
+  portNb: string; 
+  host: string;
+  password: string;
+}
+
+const dbConfig: IDbConfig = config.get('db');
+const { dbName, portNb, host, password } = dbConfig;
+
 const app = express();
 
-import user from './routes/user';
-import movie from './routes/movie';
-import room from './routes/room';
-import seat from './routes/seat';
-import screening from './routes/screening';
-import reservation from './routes/reservation';
-import login from './routes/login';
-import {isEqual} from './predicates';
+import { prodStartup } from './tools/startup/prod';
+// import user from '../routes/user';
+import movie from './modules/Movie/routes';
+// import room from '../routes/room';
+// import seat from '../routes/seat';
+// import screening from '../routes/screening';
+// import reservation from '../routes/reservation';
+// import login from '../routes/login';
+import { isEqual } from './tools/predicates';
+import { errorHandler } from './tools/middlewares';
+import {string} from 'joi';
 
 if (!config.get('jwtPrivateKey')) {
-  console.log('ERROR - jwtPrivateKey: Klucz prywatny nie został ustawiony');
+  console.log('ERROR - jwtPrivateKey: Private key not set');
   process.exit(1);
 }
 
@@ -32,28 +45,27 @@ app.use('/images', express.static('images'));
 
 /*** Routing ***/
 app.use(express.json());
-app.use('/api/user', user);
+// app.use('/api/user', user);
 app.use('/api/movie', movie);
-app.use('/api/room', room);
-app.use('/api/seat', seat);
-app.use('/api/screening', screening);
-app.use('/api/reservation', reservation);
-app.use('/api/login', login);
+// app.use('/api/room', room);
+// app.use('/api/seat', seat);
+// app.use('/api/screening', screening);
+// app.use('/api/reservation', reservation);
+// app.use('/api/login', login);
 
-app.use((error, req, res, next) => {
-  const { statusCode = 500, message, data } = error;
-  res.status(statusCode).json({ message, data });
-});
+app.use(errorHandler);
 
-if (isEqual(process.env.NODE_ENV, 'production')) require('./startup/prod')(app);
+if (isEqual(process.env.NODE_ENV, 'production')) prodStartup(app); 
+
+// if (isEqual(process.env.NODE_ENV, 'production')) require('./startup/prod')(app);
 
 const dbUri = (() => {
   const nodeEnv = process.env.NODE_ENV;
-  if (!nodeEnv) return `mongodb://${host}:${portNb}/${dbName}`;
-  else if (isEqual(nodeEnv, 'testing'))
+  if (isEqual(nodeEnv, 'testing'))
     return `mongodb://${host}:${portNb}/${dbName}`;
-  else if (isEqual(nodeEnv, 'production'))
+  if (isEqual(nodeEnv, 'production'))
     return `mongodb+srv://${host}:${password}@cinemadb-20fmo.mongodb.net/${dbName}`;
+  return `mongodb://${host}:${portNb}/${dbName}`;
 })();
 
 const port = process.env.PORT || 3001;
