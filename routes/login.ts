2d26fa2075caea18dd9eb158ba8a9c34fdabf8aa
Kamil Żogło <zoglokamil@gmail.com>
2021-09-12 12:27:45 +0200

    TS transition: - temporary deletion of files and tests not being under transition into TD app version (to generate straightforward directories structure without misunderstandings)

--- routes/login.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import loginController from '../controllers/login';
-
-/*** Endpoints ***/
-router.post('/', loginController.login);
-
-export default router;
